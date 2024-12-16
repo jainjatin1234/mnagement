7653a0ff22ae702be0ff965cf4b0424a3cf3da24
jatin jain <jainjatin748@gmail.com>
2024-12-16 22:36:26 +0530

    hjhj

--- index.js
+++ index.js
@@ -20,23 +20,14 @@ mongoose
   })
 
 const app = express();
-
-app.use(cors()); // Enable CORS for all routes
-
-// OR for specific origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:5173', // Allow only this origin
-//     methods: ['GET', 'POST'], // Allowed HTTP methods
-//   })
-// );
+app.use(cors({ origin: ["http://localhost:5173"],credentials:true}))
 
 
 
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-// app.use(cors({ origin: ["http://localhost:5173"],credentials:true}))
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000")
