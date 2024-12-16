cd88b8690f482c2a7e9e0c3d09b34a4077829b9f
jatin jain <jainjatin748@gmail.com>
2024-12-16 18:32:23 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
-dotenv.config()
+dotenv.config(
+  {
+      path:'.env'
+  }
+)
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -22,7 +26,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: ["http://localhost:5173"],credentials:true}))
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000")
 })
 
